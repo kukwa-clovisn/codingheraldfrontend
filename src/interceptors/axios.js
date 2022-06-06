629febec72227a98bdfb760a31dd2db48169a3b0
kukwa clovis <kukwaclovisngong3@gmail.com>
2022-06-06 15:22:55 +0100

    setting up image compressor

--- src/interceptors/axios.js
+++ src/interceptors/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://advancedtechacademy.herokuapp.com/";
-axios.defaults.baseURL = "http://localhost:9002";
+axios.defaults.baseURL = "https://advancedtechacademy.herokuapp.com/";
+// axios.defaults.baseURL = "http://localhost:9002";
 
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`
